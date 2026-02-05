c443e94269aeccbe6fd60c4cf9156ce321080591
SakinaNematillayeva <sakinanematillayeva2@gmail.com>
2026-02-05 16:30:24 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+var name = prompt("Enter your name") ;
+
+console.log(name)
+
+var age = prompt("Enter your age");
+
+if (age > 18 && age < 28) {
+    var age = prompt("what's your gender?");
+} else {
+    console.log("Goodbye!")
+}
+
+var gender = prompt("Enter your gender");
+
+if (gender == "female") {
+    var gender = prompt("Thank you");
+} else {
+    console.log("Have you ever been army?")
+}
+
+var army = prompt("Have you ever been to the army?");
+
+if (army == "yes"); {
+    console.log("You don't have to go")
+} else {
+    console.log("You must go")
+}
